GameOfLife/Unit-Lib

commit f20c46bac400413bc46733aedbc9c0b8f75bf8a8

load Udefs if possible

Classes/Core/Unit.sc
@@ -176,11 +176,17 @@ Udef : GenericDef {
 		defs = this.synthDef.asCollection;
 		server.asCollection.do{ |s|
 			if( s.class == LoadBalancer ) {
-				s.servers.do({ |s|
-					defs.do(_.send(s));
-				});
+				if( s.isLocal ) { 
+					defs.do(_.load(s)); 
+				} {
+					defs.do(_.send(s)); 
+				};
 			} {
-				defs.do(_.send(s));
+				if( s.isLocal ) { 
+					defs.do(_.load(s)); 
+				} {
+					defs.do(_.send(s)); 
+				};
 			};
 		}
 	}

commit 4634e036d0bb7aa4e1e7bed62ff59839451cb39e

Udef:loadSynthDef fix bug introduced by f20c46bac400413bc46733aedbc9c0b8f75bf8a8

Classes/Core/Unit.sc
@@ -179,11 +179,13 @@ Udef : GenericDef {
 		defs = this.synthDef.asCollection;
 		server.asCollection.do{ |s|
 			if( s.class == LoadBalancer ) {
-				if( s.isLocal ) { 
-					defs.do(_.load(s)); 
-				} {
-					defs.do(_.send(s)); 
-				};
+				s.servers.do({ |s|
+					if( s.isLocal ) {
+						defs.do(_.load(s));
+					} {
+						defs.do(_.send(s));
+					};
+				})
 			} {
 				if( s.isLocal ) { 
 					defs.do(_.load(s)); 